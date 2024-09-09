0a885b0fb4ca0efdb6b27e358dfc3b2569c3ff52
fannymuniz11 <munizfanny03@gmail.com>
2024-09-09 09:38:44 -0300

    atividade07

---
+++ atividade07.py
@@ -0,0 +1,7 @@
+# Enunciado: Uma loja aplica um imposto de 12% sobre o valor dos produtos. Crie um programa que receba o preço de um produto e calcule o preço final com o imposto incluído.
+
+prod = float(input("Valor do produto"))
+
+FinaldProd = float ((prod * 0.12) + prod)
+
+print (F" O valor é {FinalProd}")
